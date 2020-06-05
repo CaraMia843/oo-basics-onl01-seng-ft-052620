81601e4ad25975490c0d3acf50b62929da8cd0a0
Cara Zellner <carajeanine@gmail.com>
2020-06-05 18:07:08 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,11 +1,12 @@
 class Shoe
   
   attr_accessor :color, :size, :material, :condition
-  attr_reader :title
+  attr_reader :brand
   
   def initialize(brand)
     @brand = brand
   end
+  
   
   
 end
